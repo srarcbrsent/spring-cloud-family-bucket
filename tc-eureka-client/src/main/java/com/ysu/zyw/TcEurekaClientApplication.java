5e1322ad8e7c9284d0ee61538ff665f22eae5014
张耀武 <zhang_yao_wu1993@yeah.net>
2017-04-23 21:51:46 +0800

    spring cloud eureka client

---
+++ tc-eureka-client/src/main/java/com/ysu/zyw/TcEurekaClientApplication.java
@@ -0,0 +1,12 @@
+package com.ysu.zyw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcEurekaClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TcEurekaClientApplication.class, args);
+	}
+}
