6c75edcebed313f802528be6d1ad5dcf46703e2b
张耀武 <zhang_yao_wu1993@yeah.net>
2017-04-23 22:09:15 +0800

    spring cloud eureka client

--- tc-eureka-client/src/main/java/com/ysu/zyw/TcEurekaClientApplication.java
+++ tc-eureka-client/src/main/java/com/ysu/zyw/TcEurekaClientApplication.java
@@ -2,11 +2,14 @@ package com.ysu.zyw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class TcEurekaClientApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TcEurekaClientApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TcEurekaClientApplication.class, args);
+    }
+
 }
